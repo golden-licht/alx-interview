47cf012b3a13537b89bb14d535df71e990e6f239
golden-licht <luminoonimul@gmail.com>
2024-11-16 13:07:53 +0300

    Starwars api

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+const request = require('request');
+
+const filmId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${filmId}/`;
+console.log('Fetching character names...');
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+  } else {
+    const data = JSON.parse(body);
+    const characters = data.characters;
+
+    const characterPromises = characters.map(characterUrl => {
+      return new Promise((resolve, reject) => {
+        request(characterUrl, (error, response, body) => {
+          if (error) {
+            reject(error);
+          } else {
+            const characterData = JSON.parse(body);
+            resolve(characterData.name);
+          }
+        });
+      });
+    });
+
+    Promise.all(characterPromises)
+      .then(names => {
+        names.forEach(name => console.log(name));
+      })
+      .catch(err => console.error('Error fetching character names:', err));
+  }
+});
